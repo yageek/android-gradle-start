f7fa0fbfe4cdd13c7a72865f883c0a249540ec9f
HEINRICH Yannick <yannick.heinrich@gmail.com>
2013-07-18 17:26:56 +0200

    Add base step

---
+++ src/main/java/com/thirstyturtle/Main.java
@@ -0,0 +1,13 @@
+package com.thirstyturtle;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Main extends Activity {
+
+  @Override
+  public void onCreate( Bundle savedInstanceState ) {
+    super.onCreate( savedInstanceState );
+  }
+
+}
